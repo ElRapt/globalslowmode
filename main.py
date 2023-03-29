c55f7a060c9d8eac0abcc5ccf9500654182d94ce
Lucas Denis <lucas.denis@etu.uca.fr>
2023-03-29 20:22:25 +0200

    Improved security

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ credentials = json.load(open('credentials.json'))
 class MyClient(discord.Client):
     async def on_ready(self):
         tree.isSlowed=False;
-        await tree.sync(guild=discord.Object(id=514484765110435840))
+        await tree.sync(guild=discord.Object(id=credentials.get('guild_id')))
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
@@ -37,16 +37,16 @@ intents.message_content = True
 client = MyClient(intents=intents)
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "hello", description = "Says hello", guild=discord.Object(id=514484765110435840)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "hello", description = "Says hello", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
 
-@tree.command(name = "hidden", description = "Says hello, but hidden", guild=discord.Object(id=514484765110435840)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "hidden", description = "Says hello, but hidden", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Hello!", ephemeral=True)
 
 #Command that starts a timer of 10 seconds using the library time and writes "Timer ended" when it's done.
-@tree.command(name = "slowmode", description = "Starts slowmode for X seconds", guild=discord.Object(id=514484765110435840)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "slowmode", description = "Starts slowmode for X seconds", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction, seconds:int):
     try:
         seconds = int(seconds)
@@ -60,7 +60,7 @@ async def first_command(interaction, seconds:int):
     await interaction.followup.send("Timer ended!", ephemeral=True)
 
 
-@tree.command(name = "revoke", description = "Ends slowmode", guild=discord.Object(id=514484765110435840)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "revoke", description = "Ends slowmode", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Slowmode revoked", ephemeral=True)
     tree.isSlowed=False;
