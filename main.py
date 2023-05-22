6e84beb66a5171a610e041c940d9b10f80187d2a
Lucas Denis <lucas.denis@etu.uca.fr>
2023-05-22 15:14:35 +0200

    Added support for multiple servers

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import discord
 import json
 from discord import app_commands
@@ -8,45 +7,48 @@ import asyncio
 credentials = json.load(open('credentials.json'))
 
 class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.server_settings = {}  # Initialize the settings dictionary
+
+    def get_server_settings(self, guild_id):
+        if guild_id not in self.server_settings:
+            self.server_settings[guild_id] = {
+                'isSlowed': False,
+                'activeSlow': False,
+                'slowTime': 0,
+                'embedSlow': False,
+                'slowMessage': "Slowmode activated"
+            }
+        return self.server_settings[guild_id]
+
     async def on_ready(self):
-        tree.isSlowed=False;
-        tree.activeSlow=False;
-        tree.slowTime = 0;
-        tree.embedSlow = False;
-        tree.slowMessage = "Slowmode activated"
-        await tree.sync(guild=discord.Object(id=credentials.get('guild_id')))
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-
     async def on_message(self, message):
-        if tree.activeSlow == True:
-            if tree.isSlowed == False:
-                tree.isSlowed=True;
-                await asyncio.sleep(tree.slowTime)
-                tree.isSlowed=False;
+        settings = self.get_server_settings(message.guild.id)
+        if settings['activeSlow'] == True:
+            if settings['isSlowed'] == False:
+                settings['isSlowed'] = True
+                await asyncio.sleep(settings['slowTime'])
+                settings['isSlowed'] = False
             else:
                 if message.author.id == self.user.id:
                     return
-                await message.author.send(tree.slowMessage)
+                await message.author.send(settings['slowMessage'])
                 await message.delete()
-        elif tree.embedSlow == True:
-            if (message.embeds or message.attachments):
-                if tree.isSlowed == False:
-                    tree.isSlowed=True;
-                    await asyncio.sleep(tree.slowTime)
-                    tree.isSlowed=False;
+        elif settings['embedSlow'] == True:
+            if message.embeds or message.attachments or 'https://vxtwitter.com/' in message.content:
+                if settings['isSlowed'] == False:
+                    settings['isSlowed'] = True
+                    await asyncio.sleep(settings['slowTime'])
+                    settings['isSlowed'] = False
                 else:
                     if message.author.id == self.user.id:
                         return
-                    await message.author.send(tree.slowMessage)
+                    await message.author.send(settings['slowMessage'])
                     await message.delete()
-
-
-
-        #if message.content.startswith('!ping'):
-         #   await message.response.send_message(content="Only you can see this !", ephemeral=True)
-
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -54,43 +56,45 @@ intents.message_content = True
 client = MyClient(intents=intents)
 tree = app_commands.CommandTree(client)
 
-
-@tree.command(name = "slowmode", description = "Launches slow mode", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "slowmode", description = "Launches slow mode") 
 @has_permissions(administrator = True)
 async def first_command(interaction, seconds:int, message:str):
+    settings = client.get_server_settings(interaction.guild.id)
     try:
-        tree.slowTime = int(seconds)
-        tree.slowMessage = message
+        settings['slowTime'] = int(seconds)
+        settings['slowMessage'] = message
     except ValueError:
         await interaction.response.send_message("Enter a valid time", ephemeral=True)
         return
     await interaction.response.send_message("Slow mode activated", ephemeral=True)
-    tree.activeSlow=True;
+    settings['activeSlow'] = True
 
-@tree.command(name = "revoke", description = "Ends true slowmode", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "revoke", description = "Ends true slowmode") 
 @has_permissions(administrator = True)
 async def first_command(interaction):
+    settings = client.get_server_settings(interaction.guild.id)
+    settings['activeSlow'] = False
     await interaction.response.send_message("Slowmode revoked", ephemeral=True)
-    tree.activeSlow=False;
 
-
-@tree.command(name = "embedslow", description = "Launches slow mode for embeds", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "embedslow", description = "Launches slow mode for embeds")
 @has_permissions(administrator = True)
 async def first_command(interaction, seconds:int, message:str):
+    settings = client.get_server_settings(interaction.guild.id)
     try:
-        tree.slowTime = int(seconds)
-        tree.slowMessage = message
+        settings['slowTime'] = int(seconds)
+        settings['slowMessage'] = message
     except ValueError:
         await interaction.response.send_message("Enter a valid time", ephemeral=True)
         return
     await interaction.response.send_message("Embed slow activated", ephemeral=True)
-    tree.embedSlow=True;
+    settings['embedSlow'] = True
 
-@tree.command(name = "embedrevoke", description = "Ends true slowmode for embeds", guild=discord.Object(id=credentials.get('guild_id'))) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "embedrevoke", description = "Ends true slowmode for embeds")
 @has_permissions(administrator = True)
 async def first_command(interaction):
+    settings = client.get_server_settings(interaction.guild.id)
+    settings['embedSlow'] = False
     await interaction.response.send_message("Embed slowmode revoked", ephemeral=True)
-    tree.embedSlow=False;
-
 
 client.run(credentials.get('token'))
+
