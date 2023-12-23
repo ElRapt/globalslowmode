b02e858d60a3bc3889be58ebcbb59136f3856e4b
ElRapt <lucas.denis@etu.uca.fr>
2023-12-23 11:44:09 +0100

    Added reconnect loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 from bot import MyBot
 from utils.credentials import load_credentials
 from database.init import init_db, reset_isSlowed_for_all_servers
@@ -11,4 +12,28 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = MyBot(intents=intents)
 
-bot.run(credentials['token'])
+async def reconnect_loop():
+    while True:
+        await asyncio.sleep(1)
+        if bot.is_closed():
+            print('Bot is disconnected, attempting to reconnect...')
+            try:
+                await bot.start(credentials['token'])
+                print('Reconnect successful.')
+                break
+            except Exception as e:
+                print(f'Reconnect failed, {e}, retrying in 5 seconds...')
+                await asyncio.sleep(5)
+
+async def main():
+    try:
+        await bot.start(credentials['token'])
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        print(f"Fatal exception {e}, running reconnect loop.")
+        await reconnect_loop()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
